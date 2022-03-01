73ba2e76c9134c84f2bbddd0220430beeeba3ec7
gebeto <slavik.nychkalo@gmail.com>
2022-03-02 01:59:54 +0200

    add training model

--- test.py
+++ test.py
@@ -4,7 +4,8 @@ from spacy.matcher import Matcher
 # nlp = spacy.load("xx_ent_wiki_sm")
 nlp = spacy.load("./output/model-last")
 # doc = nlp("Today I was in Lviv ant tomorrow I will go to Kyiv")
-doc = nlp("Yesterday I steal F22")
+doc = nlp("Поїду з Яворова (левада, черкаси) завтра до Львова в 12:00, 0970067238")
+# doc = nlp("Підвезу до Львова з Яворова о 7:00 (ц.Анни)")
 
 for ent in doc.ents:
   print(
